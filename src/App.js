9d6f07ff5b582db6e27d930da423c31dc5bca27f
unknown <sagarsahu0110@gmail.com>
2023-05-04 01:04:51 +0530

    this is otp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import OtpGenerate from'./component/Otp';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <OtpGenerate/>
+    </div>
+  );
+}
+
+export default App;
